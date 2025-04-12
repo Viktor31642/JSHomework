34ad76cfde83c375f6db0247c7cc092f5f43f8a8
Viktor31642 <viktor31642@gmail.com>
2025-04-12 16:49:45 +0300

    creat_homework1

---
+++ index.js
@@ -0,0 +1,121 @@
+
+// task 2
+
+// console.log("Krupin")
+
+
+// task 3
+
+
+// let name;
+// let surname;
+
+
+// name = 'Viktor';
+// surname = 'Krupin';
+
+
+// console.log('name:', name);
+// console.log('surname:', surname);
+
+
+// name = surname;
+
+
+// console.log(name);
+// console.log(surname);
+
+
+
+
+// task 4
+
+
+// const x = {
+//     string: "Це рядок",         
+//     number: 2025,               
+//     boolean: true,              
+//     undefined: undefined,       ??????
+//     null: null                
+//   };
+
+//   console.log(x);
+
+
+
+// task 5
+
+
+// const isAdult = confirm("Вам вже є 18 років?");
+
+
+// console.log("Користувач повнолітній:", isAdult);
+
+
+// task 6
+
+
+// let name = "Viktor";        // string
+// let surname = "Krupin";          // string
+// let studyGroup = "AFH";          // string
+// let birthYear = 1997;               // number
+// let doYouHaveChildren;         //underfined
+// let birthDay = null;           //null
+
+
+
+// let isMarried = true;              // boolean
+
+// console.log(birthYear);
+// console.log(isMarried);
+// console.log(name);
+// console.log(surname);
+// console.log(studyGroup);
+// console.log(doYouHaveChildren);
+// console.log(birthDay);
+
+
+
+// task 7
+
+// let userLogin = prompt("Введіть ваш логін:");
+// let userEmail = prompt("Введіть ваш email:");
+// let userPassword = prompt("Введіть ваш пароль:");
+
+// let message = `Dear ${userLogin}, your email is ${userEmail}, your password is ${userPassword}`;
+
+// alert(message);
+
+
+
+// task 8
+
+// const secondsInMinute = 60;
+// const minutesInHour = 60;
+// const hoursInDay = 24;
+
+
+// const secondsInHour = secondsInMinute * minutesInHour;
+// const secondsInDay = secondsInHour * hoursInDay;
+
+
+// const userDaysInMonth = prompt("Скільки днів у місяці ви хочете врахувати?");
+// const daysInMonth = Number(userDaysInMonth);
+
+
+// const secondsInMonth = secondsInDay * daysInMonth;
+
+
+// alert(`Секунд у годині: ${secondsInHour}
+// Секунд у добі: ${secondsInDay}
+// Секунд у місяці (${daysInMonth} днів): ${secondsInMonth}`);
+
+// console.log("Секунд у годині:", secondsInHour);
+// console.log("Секунд у добі:", secondsInDay);
+// console.log(`Секунд у місяці (${daysInMonth} днів):`, secondsInMonth);
+
+
+
+
+
+
